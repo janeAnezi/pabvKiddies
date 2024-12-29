e82c32bca168061fdff7665dbc4f2104f8702369
Anezijane <ahnnieanezi@gmail.com>
2024-12-30 00:02:24 +0100

    add homecontent

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,19 @@ const slides = [
         id: '1',
         image: hero1,
         title: "Hero 1",
-        description: "This is the first hero"
+        description: "This is the first slide"
     },
     {
         id: '2',
         image: hero2,
         title: "Hero 2",
-        description: "This is the first hero"
+        description: "This is the second slide"
     },
     {
         id: '3',
         image: hero3,
         title: "Hero 3",
-        description: "This is the first hero"
+        description: "This is the third slide "
     },
 ]
 
@@ -34,7 +34,7 @@ const App = () => {
                     <div style={{backgroundImage: `url(${slide.image})`, backgroundSize: 'cover',
                     backgroundPosition: 'center',
                     width: '100%',
-                    height: '300px',}} key={slide.id}>
+                    height: '421px',}} key={slide.id}>
                     
                         <div className="w-full h-full bg-black/70 flex flex-col items-center justify-center text-white">
                             <h2>{slide.title}</h2>
--- src/Component/Carousel.jsx
+++ src/Component/Carousel.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import { IoIosArrowForward,  IoIosArrowBack  } from "react-icons/io";
 
 const Carousel = ({children: slides}) => {
+    const [currSlide, setCurrSlide] = useState(0)
+    const next = () => {
+        setCurrSlide((current) => {current === slides.length - 1 ? 0 : currSlide + 1 })
+    }
+    const prev = () => {
+        setCurrSlide((current) => {current === currSlide ? slides.length - 1 : currSlide - 1})
+    }
   return (
     <div className='w-screen overflow-hidden relative'>
-      <div className='flex'>{slides}</div>
-        <div className="absolute inset-0 flex items-center justify-between text-slate-500 ">
-            <button>
-                < IoIosArrowBack size={30}/>
+      <div className='flex transition-transform ease-out duration-700' style={{transform: `translateX(-${currSlide * 100}%)`}}>{slides}</div>
+        <div className="absolute inset-0 flex items-center justify-between text-slate-500 p-4 ">
+            <button onClick={prev}>
+                < IoIosArrowBack size={30} />
             </button>
-            <button>
+            <button onClick={next}>
                 <IoIosArrowForward size={30}/>
             </button>
         </div>

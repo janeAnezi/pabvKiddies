26d592b32695a5035a7d056a04dceb99a5066e91
Anezijane <ahnnieanezi@gmail.com>
2024-12-29 23:36:31 +0100

    add homecontent

--- src/Component/Carousel.jsx
+++ src/Component/Carousel.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { IoIosArrowForward,  IoIosArrowBack  } from "react-icons/io";
 
 const Carousel = ({children: slides}) => {
   return (
     <div className='w-screen overflow-hidden relative'>
       <div className='flex'>{slides}</div>
+        <div className="absolute inset-0 flex items-center justify-between text-slate-500 ">
+            <button>
+                < IoIosArrowBack size={30}/>
+            </button>
+            <button>
+                <IoIosArrowForward size={30}/>
+            </button>
+        </div>
     </div>
   )
 }

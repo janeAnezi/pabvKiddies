daa1fd8978e3851a89990a51c3bb1f3cd5f87483
Anezijane <ahnnieanezi@gmail.com>
2025-01-01 23:32:29 +0100

    add footer socials

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,3 +1,8 @@
+import { FaFacebookF } from "react-icons/fa";
+import { SlSocialInstagram } from "react-icons/sl";
+import { FaXTwitter } from "react-icons/fa6";
+import { BsWhatsapp } from "react-icons/bs";
+
 const Footer = () => {
     return (
       <footer className="bg-gray-800 text-white py-10">
@@ -23,24 +28,24 @@ const Footer = () => {
               <h3 className="text-lg font-semibold mb-4">Contact Us</h3>
               <ul className="text-sm text-gray-400 space-y-2">
                 <li>Email: <a href="mailto:info@pabvkiddies.com" className="hover:text-orange-500">info@pabvkiddies.com</a></li>
-                <li>Phone: <a href="tel:+234123456789" className="hover:text-orange-500">+234 123 456 789</a></li>
-                <li>Address: 123 Kiddies Lane, Abuja, Nigeria</li>
+                <li>Phone: <a href="tel:+234123456789" className="hover:text-orange-500">+234 806 387 6631</a></li>
+                <li>Address: 123 Onuiyi nsukka, Enugu. Nigeria</li>
               </ul>
             </div>
             <div>
               <h3 className="text-lg font-semibold mb-4">Follow Us</h3>
               <div className="flex space-x-4">
                 <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                  <i className="fab fa-facebook text-2xl hover:text-orange-500"></i>
+                  <i className="text-2xl text-orange-500 hover:text-orange-100"><FaFacebookF/></i>
                 </a>
                 <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                  <i className="fab fa-twitter text-2xl hover:text-orange-500"></i>
+                  <i className="text-2xl text-orange-500 hover:text-orange-100"><FaXTwitter/></i>
                 </a>
                 <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                  <i className="fab fa-instagram text-2xl hover:text-orange-500"></i>
+                  <i className="text-2xl text-orange-500 hover:text-orange-100"><SlSocialInstagram/></i>
                 </a>
-                <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                  <i className="fab fa-linkedin text-2xl hover:text-orange-500"></i>
+                <a href="https://whatsappme.com" target="_blank" rel="noopener noreferrer">
+                  <i className="text-2xl text-orange-500 hover:text-orange-100"><BsWhatsapp/></i>
                 </a>
               </div>
             </div>

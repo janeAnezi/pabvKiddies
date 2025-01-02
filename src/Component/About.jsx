fd22d31684caaafdd58198accf17b32f53957cdf
Anezijane <ahnnieanezi@gmail.com>
2025-01-02 22:24:09 +0100

    add about section

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <section className="min-h-screen">
       <div className="text-center mb-10 w-full object-contain">
         <video src={pabvid} autoPlay loop muted className='w-full h-full'/>
-        <div className="relative z-10 flex flex-col items-center justify-center h-full">
+        <div className="absolute top-0 w-full flex flex-col items-center justify-center h-full">
           <h2 className="text-4xl font-bold text-orange-500">About Us</h2>
           <p className="text-gray-200 mt-4 max-w-2xl">
             Explore the creativity and elegance of PABVkiddies, where African heritage meets modern designs for kids.

364a9bc048353051b82e902dc0bd301de3101aa3
Anezijane <ahnnieanezi@gmail.com>
2025-01-02 23:12:16 +0100

    add flip card about section

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import pabvid from '../assets/img/pabvid.mp4';
+import FlipCard from './Card';
 
 const About = () => {
   return (
     <section className="min-h-screen">
-      <div className="text-center mb-10 w-full object-contain">
-        <video src={pabvid} autoPlay loop muted className='w-full h-full'/>
-        <div className="absolute top-0 w-full flex flex-col items-center justify-center h-full">
-          <h2 className="text-4xl font-bold text-orange-500">About Us</h2>
-          <p className="text-gray-200 mt-4 max-w-2xl">
-            Explore the creativity and elegance of PABVkiddies, where African heritage meets modern designs for kids.
-          </p>
+        <div className="text-center mb-10 w-full object-contain">
+            <video src={pabvid} autoPlay loop muted className='w-full h-full'/>
+            <div className="absolute top-0 w-full flex flex-col items-center justify-center h-full">
+                <h2 className="text-4xl font-bold text-orange-500">About Us</h2>
+                <p className="text-gray-200 mt-4 max-w-2xl">
+                    Explore the creativity and elegance of PABVkiddies, where African heritage meets modern designs for kids.
+                </p>
+            </div>
         </div>
-      </div>
+        <div>
+            <FlipCard/>
+        </div>
     </section>
   );
 };

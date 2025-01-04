2595d53f488a2c55022b9744409dbe7f71c8f834
Anezijane <ahnnieanezi@gmail.com>
2025-01-04 22:37:26 +0100

    update about content

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import pabvid from '../assets/img/pabvid.mp4';
+import sketch1 from '../assets/img/sketch1.png'
+import sketch2 from '../assets/img/sketch2.png'
+import sketch3 from '../assets/img/sketch3.png'
 
 const About = () => {
   return (
@@ -22,12 +25,40 @@ const About = () => {
         </div>
       </div>
 
-      <div>
-        <h2 className="text-3xl font-bold text-orange-500">Our Mission</h2>
-        <p>We bring Ankara fashion to life for kids.</p>
-        <p>At PABVkiddies, we believe in blending culture with creativity. 
-      Our mission is to create beautiful, comfortable, and stylish outfits 
-      for kids that celebrate African heritage.</p>
+      <div> 
+        <div className='flex justify-center items-center gap-8 py-5'>
+            <div className='w-60'><img src={sketch1} alt="" /></div>
+            <div className='w-80'>
+                <h2 className="text-3xl font-bold text-orange-500">Our Mission</h2>
+                <p className='text-sky-400'>We bring Ankara fashion to life for kids.</p>
+                <p>At PABVkiddies, we believe in blending culture with creativity. 
+                    Our mission is to create beautiful, comfortable, and stylish outfits 
+                    for kids that celebrate African heritage.
+                </p>
+            </div>
+        </div>
+        <div className='flex justify-center items-center gap-8 bg-orange-50 py-10'>
+            <div className='w-80'>
+                <h2 className="text-3xl font-bold text-orange-500">Why Choose Us?</h2>
+                <p className='text-sky-400'>Quality, Style, and Affordability.</p>
+                <p>Unique Ankara designs crafted for kids.
+                    - Premium materials for comfort and durability.
+                    - Affordable prices for every family.
+                </p>
+            </div>
+            <div className='w-60'><img src={sketch2} alt="" className='rounded-xl' /></div>
+        </div>
+        <div className='flex justify-center items-center gap-8 py-5'>
+            <div className='w-60' ><img src={sketch3} alt="" /></div>
+            <div className='w-80'>
+                <h2 className="text-3xl font-bold text-orange-500">Our Services</h2>
+                <p className='text-sky-400'>Tailored to perfection.</p>
+                <p>Bespoke Ankara outfits for kids.
+                    - Premium tailoring materials.
+                    - Exclusive fashion styles for special occasions.
+                </p>
+            </div>
+        </div>
       </div>
     </section>
   );

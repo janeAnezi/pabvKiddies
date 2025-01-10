b31dc641378d5c7bc31d4afbf509a5ecf450dd76
Anezijane <ahnnieanezi@gmail.com>
2025-01-10 22:11:34 +0100

    add button to add to cart

--- src/Component/Products.jsx
+++ src/Component/Products.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { products } from './content';
 import { TbCurrencyNaira } from "react-icons/tb";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Products = () => {
   return (
@@ -12,10 +13,10 @@ const Products = () => {
             <Link to={`/product/${product.id}`} key={product.id} className="border p-4 rounded-lg shadow-lg">
               <img src={product.image} alt={product.name} className="w-full h-40 object-cover rounded-lg" />
               <h3 className="text-md font-semibold mt-4">{product.name}</h3>
-              <p className="text-gray-500 text-sm pt-2">{product.description}</p>
-              <div className='flex justify-between py-4 px-8'>
+              <p className="text-gray-500 text-xs pt-2">{product.description}</p>
+              <div className='flex justify-between pt-4 px-8'>
                 <p className="text-gray-600 flex flex-row items-center text-sm"><TbCurrencyNaira className="text-lg"/>{product.price}</p>
-                <button className='bg-sky-500 hover:bg-sky-600 px-2 rounded-lg text-white text-xs'>Add to cart</button>
+                <button className='flex flex-row items-center gap-1.5 bg-sky-500 hover:bg-sky-600 px-2 rounded-lg text-white text-xs'><FaShoppingCart /> Add to cart</button>
               </div>
               
               

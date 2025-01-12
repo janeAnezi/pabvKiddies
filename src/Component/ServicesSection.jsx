6a4ca1ee3de08299580d4675c9d17d6a5ff50336
Anezijane <ahnnieanezi@gmail.com>
2025-01-12 23:52:49 +0100

    add swiper slide and overlay to service section component

--- src/Component/ServicesSection.jsx
+++ src/Component/ServicesSection.jsx
@@ -4,6 +4,7 @@ import 'swiper/css/pagination'
 import 'swiper/css/free-mode'
 import {FreeMode, Pagination} from 'swiper/modules'
 import { services } from './content';
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 const ServicesSection = () => {
   return (
@@ -33,6 +34,11 @@ const ServicesSection = () => {
                     <div className='flex flex-col group relative shadow-xl text-white cursor-pointer px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px]'>
                         <div className='absolute inset-0 bg-cover bg-center rounded-lg' style={{backgroundImage: `url(${item.image})`}} />
                         <div className='absolute inset-0 bg-black opacity-10 group-hover:opacity-50 rounded-lg' />
+                        <div className='relative flex flex-col gap-3'>
+                            <h1 className='text-xl lg:text-2xl'>{item.title}</h1>
+                            <p className='text-sm lg:text-lg'>{item.description}</p>
+                            <FaLongArrowAltRight className='absolute -bottom-14 left-5 w-[35px] h-[35px] text-white group-hover:text-sky-500 -rotate-45 group-hover:rotate-[2deg] duration-100'/>
+                        </div>
                     </div>
                 </SwiperSlide>
             ))}
